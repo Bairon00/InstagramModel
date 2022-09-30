90bb97c00f5db2c9eab384f033f0fbe803a6e518
Bairon00 <bairon97@icloud.com>
2022-09-30 02:07:07 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -45,43 +45,9 @@ class Media(Base):
     Url= Column(String(250),ForeignKey(Post.user_ID))
     post_id= Column(Integer, nullable=False)
     rel = relationship(Post)
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Person(Base):
-#     __tablename__ = 'person'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
-
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
 render_er(Base, 'diagram.png')
